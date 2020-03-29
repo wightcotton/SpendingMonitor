a5e2acc3d4d431eb3ffb66d8b6b17c53e8182f4e
bob <bob@wightcotton.com>
2020-03-29 13:47:47 -0500

    remove register code - use dev branch?

--- app/models.py
+++ app/models.py
@@ -1,8 +1,7 @@
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-
-
+import datetime
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,3 +22,12 @@ class User(UserMixin, db.Model):
 def load_user(id):
     return User.query.get(int(id))
 
+class UploadedFile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(64))
+    data = db.Column(db.LargeBinary)
+    timestamp = db.Column(db.DATETIME, index=True, default=datetime.datetime.utcnow())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<UploadedFile {}>'.format(self.filename)
--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,26 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
+from config import Config
 from app.forms import LoginForm, RegistrationForm, UploadForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
-from app.working_data import DatasetFactory
+from app.models import User, UploadedFile
+from app.working_data import DataObjectFactory, Budget, Transactions
 from werkzeug.urls import url_parse
 from werkzeug.utils import secure_filename
+from io import BytesIO
 
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Miguel'}
     return render_template('index.html', title='Home')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect( url_for('index'))
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -33,10 +34,12 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
@@ -52,19 +55,48 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in Config.ALLOWED_EXTENSIONS
+
 
 @app.route('/upload_file', methods=['GET', 'POST'])
 @login_required
 def upload_file():
-    form=UploadForm()
+    form = UploadForm()
     if form.validate_on_submit():
         filename = secure_filename(form.file.data.filename)
-        t = Transactions(form.file.data)
-        flash('file is: ' + t.base_file)
+        data = form.file.data.read()
+        uploaded_file = UploadedFile(filename=filename, data=data, user_id=current_user.id)
+        print(uploaded_file)
+        print(db.session.add(uploaded_file))
+        print(db.session.commit())
+        flash('file is: ' + filename)
         return redirect(url_for('index'))
     return render_template('upload_file.html', title='Upload file with budget data', form=form)
 
 
+@app.route('/monthly_drill_down')
+@login_required
+def monthly_drill_down():
+    uploaded_files = UploadedFile.query.filter_by(user_id=current_user.id)
+    f = BytesIO(uploaded_files[-1].data)
+    dfact = DataObjectFactory(uploaded_files[-1].filename, f)
+    budget_df = dfact.get_budgets_dataframe()
+    trans_df = dfact.get_trans_dataframe()
 
+    budget_info = budget_df.budget_info
+    actuals_dict = trans_df.accumulated_spending_by_category_by_month
+    summary_dict = trans_df.accumulated_spending_by_category
+    past_year_dict = trans_df.accumulated_spending_for_past_year
+    super_cat_dict = trans_df.accumulated_spending_by_super_category
+    category_dict = trans_df.accumulated_spending_by_category
+    transactions_dict = trans_df.transactions
+    return render_template('monthly_drill_down.html',
+                           actuals=actuals_dict,
+                           budgets=budget_info,
+                           summary=summary_dict,
+                           past_year=past_year_dict,
+                           super_categories=super_cat_dict,
+                           categories=category_dict,
+                           transactions=transactions_dict)
